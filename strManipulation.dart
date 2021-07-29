c6b0ebe07aa93fa9e725af92fcc3e8a12e6fcf17
fighteros <fighteros121212@gmail.com>
2021-07-29 16:19:45 +0200

    str done

---
+++ strManipulation.dart
@@ -0,0 +1,14 @@
+void main() {
+
+  // string manipulation
+  String fName = "Ahmed";
+  String lName = "Abd El-Ghany";
+  // special char escape
+  String carName = "Ahmed's car or Ahmed\"s car";
+  String name = fName + lName;
+  // dart string interpolation is like python interpolation
+  String welcomeMsg = "Hello $fName $lName name length = ${fName.length + lName.length}";
+
+  print(name);
+  print(welcomeMsg);
+}
