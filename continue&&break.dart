21ae5c369a658f8ef630366370d5e8796f914b2e
fighteros <fighteros121212@gmail.com>
2021-07-29 18:52:18 +0200

    naming loops

--- continue&&break.dart
+++ continue&&break.dart
@@ -11,12 +11,15 @@ void main() {
     }
   }
 
-
   // continue
-  for(int x = 0; x <10;x++){
-    if(x == 7){
-      continue;
+  outerLoop:
+  for (int y = 0; y < 4; y++) {
+    innerLoop:
+    for (int x = 0; x < 3; x++) {
+      if (y == 2 && x == 0) {
+        continue outerLoop;
+      }
+      print("$y - $x");
     }
-    print(x);
   }
 }
