f71054a2368277f63ae2d7f3b638b83ec4715506
fighteros <fighteros121212@gmail.com>
2021-07-29 16:04:27 +0200

    finished vars

---
+++ vars.dart
@@ -0,0 +1,30 @@
+void main() {
+  // name convention is camelCase
+  // Integers
+  int num = 25;
+  var varNum = 15;
+  print(num+varNum);
+  
+  // floating numbers
+  double number = 10.5;
+  var doubleVar = 25.5;
+  print(number+ doubleVar);
+  
+  // Strings
+  String name = "Ahmed";
+  print(name);
+
+  var strVar = "ALi";
+  print(strVar);
+
+  // booleans
+  bool isUser = true;
+  bool isAdmin = false;
+
+  var isSick = false;
+
+  print(isUser);
+  print(isAdmin);
+  print(isSick);
+
+}
