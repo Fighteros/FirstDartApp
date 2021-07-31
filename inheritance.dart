09d935cb08b9292f9700f11a060610b60ede5724
fighteros <fighteros121212@gmail.com>
2021-07-31 06:12:35 +0200

    finished all constructor in inheritance

--- inheritance.dart
+++ inheritance.dart
@@ -1,11 +1,12 @@
 void main() {
-  var human = Student(21,"Ahmed" ,"high school");
+  var human = Student(21, "Ahmed", "high school");
   print(human.studyAt);
 }
 
 class Person {
   int? age;
   String? name;
+
   Person(this.age, this.name);
 
   void eat() {}
@@ -15,7 +16,10 @@ class Student extends Person {
   String? studyAt;
 
   // easy way
-  Student(int age,String name , this.studyAt) : super(age, name);
+  Student(int age, String name, this.studyAt) : super(age, name);
+
+  // named constructor
+  Student.customStudent(int age, String name) : super(age, name);
 
   void learn() {
     print("learning");
@@ -29,5 +33,3 @@ class Teacher extends Person {
 
   void teach() {}
 }
-
-
